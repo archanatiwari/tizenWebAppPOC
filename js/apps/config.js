62c07bd3e5b89f533a70af24f08990451f84577c
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-17 16:22:27 +0530

    updated app folder stucture

---
+++ js/apps/config.js
@@ -0,0 +1,17 @@
+//var app = angular.module('myApp', ['ui.router']);
+//
+//app.config(function($stateProvider, $urlRouterProvider) {
+//    $urlRouterProvider.otherwise('landing');
+//    $stateProvider
+//        .state('landing', {
+//            url: '/landing',
+//            templateUrl: 'partials/landing.html',
+//            controller: 'landingController'
+//        })
+//        .state('login', {
+//            url: '/login',
+//            templateUrl: 'partials/login.html',
+//            controller: 'loginController'
+//        })
+//
+//});
