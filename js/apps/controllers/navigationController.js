77577613ae9f614e754deb4f61a50e49f69f681f
charan0514 <shree.adiga@gmail.com>
2016-03-23 14:33:14 +0530

    Updated NAvigation Controller

--- js/apps/controllers/navigationController.js
+++ js/apps/controllers/navigationController.js
@@ -1,5 +1,5 @@
 app.controller('navigationController', function($scope, $interval, GetUserData, SharedDataService) {
-    $scope.next = 0;
+	var next = -1, totalPplMeet = 0, tracker;
     $scope.targetLocation = SharedDataService.getTargetData();
 
     var onSuccess = function(response) {
@@ -23,24 +23,88 @@ app.controller('navigationController', function($scope, $interval, GetUserData, 
     });
 
     $scope.loadDirections = function() {
-        var directionsDisplay = new google.maps.DirectionsRenderer({
-            map: map
-        });
+    	
+    	var userList = $scope.userList;
+	    
+    	userList.forEach(function(value,index){
+    	
+	        var directionsDisplay = new google.maps.DirectionsRenderer({
+	            map: map
+	        });
+	
+	        // Set destination, origin and travel mode.
+	        var request = {
+	            origin: userList[index].source,
+	            destination: $scope.targetLocation,
+	            travelMode: google.maps.TravelMode.DRIVING
+	        };
+	
+	        // Pass the directions request to the directions service.
+	        var directionsService = new google.maps.DirectionsService();
+	        directionsService.route(request, function(response, status) {
+	            if (status == google.maps.DirectionsStatus.OK) {
+	                // Display the route on the map.
+	                directionsDisplay.setDirections(response);
+	                userList[index].destination =  $scope.targetLocation;
+	                userList[index].positions = response.routes[0].overview_path; //gives intermediate coordinates to destination
+	                if(index == (userList.length-1)) //call this method once every user's intermediate coordinates are available 
+	                	$scope.trackUserPosition();
+	            }
+	        });
+    	});
+    };
+    
+    $scope.updateUserPosition = function() {
+    	var userList =  $scope.userList;
+    	var target = $scope.targetLocation;
+		userList.forEach(function(val,index){
+			var person = val;
+			var coOrdinates =  person.positions;
+			//alert("No. of Coordinates for"+person.name+ " is "+coOrdinates.length);
+			if(coOrdinates[next]){
+				
+				var curLat = coOrdinates[next].lat();
+				var curLong =  coOrdinates[next].lng();
+				var newPoint = new google.maps.LatLng(curLat, curLong);
 
-        // Set destination, origin and travel mode.
-        var request = {
-            origin: $scope.userList[0].source,
-            destination: $scope.targetLocation,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-
-        // Pass the directions request to the directions service.
-        var directionsService = new google.maps.DirectionsService();
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                // Display the route on the map.
-                directionsDisplay.setDirections(response);
-            }
-        });
+				if(person.marker){
+					//to clear older marker
+					//person.marker.setMap(null);
+					person.marker.setPosition(newPoint);
+				}
+				else{
+						person.marker = new google.maps.Marker({
+						position : newPoint,
+						map: map,
+						label: person.name,
+						//icon: imageUrl,
+						draggable: true
+					});
+				}
+				person.marker.setMap(map);
+				//map.setCenter(newPoint);
+			}
+			else{
+				if ((target.lat === person.destination.lat && target.lng === person.destination.lng) && (!person.reached)) {
+					//if(!person.reached){
+						person.reached = true;
+						totalPplMeet++;
+						//all people reached destinamtion;
+						if(totalPplMeet == userList.length){
+							next = -1;
+							$interval.cancel(tracker);
+						}
+					//}
+					
+				}	
+			}
+		});
+    };
+    
+    $scope.trackUserPosition = function(){
+    	tracker = $interval(function(){
+			next++;
+			$scope.updateUserPosition();
+		},1000);
     };
 });
