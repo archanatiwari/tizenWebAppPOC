51e13273bdd066378d2b0200c582e064d9b23b73
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-23 21:19:11 +0530

    login page controller update

--- js/apps/controllers/loginController.js
+++ js/apps/controllers/loginController.js
@@ -1,5 +1,19 @@
-app.controller('loginController' ,function($scope){
+app.controller('loginController' ,function($scope,$http,$location){
+	$scope.userContacts= [];
+	$http.get('data/user-data.json').success(function(data){
+		$scope.userContacts = data.data;
 
-      // $scope.message="This is page 2";
-
+	});
+	$scope.login= function(){
+		angular.forEach($scope.userContacts, function(user, key){
+			if(user.email==$scope.email){
+				$scope.currentUser=user.name;
+				$location.path('/contactList');
+			
+			}
+			
+		})
+	}
+	
+	
 });
