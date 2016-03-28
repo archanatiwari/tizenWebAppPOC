f50ae313b39a4dde363aaf61ad7082392703850c
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-28 21:28:50 +0530

    date format in home page

--- js/apps/controllers/homeController.js
+++ js/apps/controllers/homeController.js
@@ -1,12 +1,17 @@
-app.controller('homeController',function($scope,SharedDataService){
+app.controller('homeController', function($scope, SharedDataService) {
 
-     $scope.currentUser= SharedDataService.getCurrentUser();
-     console.log($scope.currentUser);
-     if($scope.currentUser.eventlist.Eventlist){
-     	for (var i = $scope.currentUser.eventlist.Eventlist.length - 1; i >= 0; i--) {
-     		$scope.currentUser.eventlist.Eventlist[i]
-     	};
-     }
-
+    $scope.currentUser = SharedDataService.getCurrentUser();
+    console.log($scope.currentUser);
+    
+    for (var i = 0; i < $scope.currentUser.eventlist.EventList.length; i++) {
+        var date = new Date($scope.currentUser.eventlist.EventList[i].EventDate);
+        if (date.getDate() == new Date().getDate() && date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) {
+            $scope.currentUser.eventlist.EventList[i].EventDate = "Today";
+        } else if (date.getDate() == new Date().getDate() + 1 && date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) {
+            $scope.currentUser.eventlist.EventList[i].EventDate = "Tomorrow";
+        } else {
+            $scope.currentUser.eventlist.EventList[i].EventDate = date;
+        }
+    }
 
 });
