5e2435cce1086eac2b51173384034a995415790b
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-28 22:08:18 +0530

    Upcoming and all event functionality done

--- js/apps/controllers/homeController.js
+++ js/apps/controllers/homeController.js
@@ -1,8 +1,15 @@
 app.controller('homeController', function($scope, SharedDataService) {
 
     $scope.currentUser = SharedDataService.getCurrentUser();
-    console.log($scope.currentUser);
-    
+    // console.log($scope.currentUser);
+    $scope.upcomingEvents=[];
+	for (var i = 0; i < $scope.currentUser.eventlist.EventList.length; i++){
+		var date = new Date($scope.currentUser.eventlist.EventList[i].EventDate);
+		if(date>= new Date()){
+			$scope.upcomingEvents.push($scope.currentUser.eventlist.EventList[i]);
+		}
+	}    
+
     for (var i = 0; i < $scope.currentUser.eventlist.EventList.length; i++) {
         var date = new Date($scope.currentUser.eventlist.EventList[i].EventDate);
         if (date.getDate() == new Date().getDate() && date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) {
