808964fcfeea2404a58621f2bc357fe7e0f30f45
charan0514 <shree.adiga@gmail.com>
2016-03-28 16:19:07 +0530

    changes for loading tau.js on each state change

--- js/apps/controllers/loginController.js
+++ js/apps/controllers/loginController.js
@@ -6,15 +6,19 @@ app.controller('loginController' ,function($scope,$http,$location,SharedDataServ
 
 	});
 	$scope.login= function(){
-		angular.forEach($scope.userContacts, function(user, key){
+		var userContacts = $scope.userContacts;
+		//angular.forEach($scope.userContacts, function(user, key){ //do not use forEach, as there is no option to break the loop
+		for(var i=0; i<userContacts.length;i++){
+			var user = userContacts[i];
 			if(user.email==$scope.email && user.password==$scope.password){
 				SharedDataService.setCurrentUser(user);
 				$location.path('/home');
+				break;
 			}else{
 				$scope.errorMsg=true;
 			}
-			
-		})
+		}
+		//})
 	}
 	
 	
