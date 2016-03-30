52bc4093e8b9a323495e8315bb05821f788d9588
kaushik gotefode <kaushikgotefode@gmail.com>
2016-03-30 20:16:04 +0530

    create home page data binding

--- js/apps/controllers/homeController.js
+++ js/apps/controllers/homeController.js
@@ -1,35 +1,76 @@
 app.controller('homeController', function($scope, $state, SharedDataService) {
+     $scope.eventMemberList = function(inviteeList) {
+        $scope.customisedInviteeList = "";
+        $scope.arrivedPpl = [];
+        str = "";
+
+        //doing this for customizing invitee list
+        for (var i = 0; i < inviteeList.length; i++) {
+            var invitee = inviteeList[i].name;
+            if (inviteeList.length == 1) {
+                $scope.customisedInviteeList = invitee;
+                break;
+            }
+            if (inviteeList.length <= 4) {
+                if (inviteeList[i + 1]) {
+                    str += invitee;
+                    if (inviteeList[i + 2])
+                        str += ", ";
+                } else {
+                    str += " and " + invitee;
+                    $scope.customisedInviteeList = str;
+                    break;
+                }
+            } else {
+                if (i <= 3) {
+                    str += invitee;
+                    if (i < 3)
+                        str += ", ";
+                } else {
+                    var remainingPpl = inviteeList.length - i;
+                    str += " and " + remainingPpl + " more";
+                    $scope.customisedInviteeList = str;
+                    break;
+                }
+            }
+        }
+    }
 
     $scope.currentUser = SharedDataService.getCurrentUser();
     // console.log($scope.currentUser);
-    $scope.upcomingEvents=[];
-	for (var i = 0; i < $scope.currentUser.eventList.length; i++){
-		if($scope.currentUser.eventList[i].mystatus=="ACCEPTED"){
-			$scope.currentUser.eventList[i].statusClass= "accepted";
-		} else if($scope.currentUser.eventList[i].mystatus=="PENDING"){
-			$scope.currentUser.eventList[i].statusClass= "pending";
-		}else{
-			$scope.currentUser.eventList[i].statusClass= "rejected";
-		}
-		var date = new Date($scope.currentUser.eventList[i].eventDate);
-		if(date>= new Date()){
-			$scope.upcomingEvents.push($scope.currentUser.eventList[i]);
-		}
-	}    
+    $scope.upcomingEvents = [];
+    for (var i = 0; i < $scope.currentUser.eventList.length; i++) {
+        if ($scope.currentUser.eventList[i].mystatus == "ACCEPTED") {
+            $scope.currentUser.eventList[i].statusClass = "accepted";
+        } else if ($scope.currentUser.eventList[i].mystatus == "PENDING") {
+            $scope.currentUser.eventList[i].statusClass = "pending";
+            $scope.eventMemberList($scope.currentUser.eventList[i].inviteeList);
+        } else {
+            $scope.currentUser.eventList[i].statusClass = "rejected";
+        }
+        
+        var date = new Date($scope.currentUser.eventList[i].eventDate);
+        if (date >= new Date()) {
+            $scope.upcomingEvents.push($scope.currentUser.eventList[i]);
+        }
+    }
 
     for (var i = 0; i < $scope.currentUser.eventList.length; i++) {
         var date = new Date($scope.currentUser.eventList[i].eventDate);
         if (date.getDate() == new Date().getDate() && date.getMonth() == new Date().getMonth() && date.getFullYear() == new Date().getFullYear()) {
             $scope.currentUser.eventList[i].eventDate = "Today";
-        }else {
+        } else {
             $scope.currentUser.eventList[i].eventDate = date;
         }
     };
-    
-    $scope.getEventDetails = function(eventObj){
-    	SharedDataService.setEventData(eventObj);
-    	//SharedDataService.setTargetData(eventObj.destination);
-    	$state.go('navigation');
+
+
+    $scope.getEventDetails = function(eventObj) {
+        SharedDataService.setEventData(eventObj);
+        //SharedDataService.setTargetData(eventObj.destination);
+        $state.go('navigation');
     }
+
+   
 
 });
